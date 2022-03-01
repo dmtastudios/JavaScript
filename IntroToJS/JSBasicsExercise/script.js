d97a78f14f7b7839d95fef2395fc8658d28b6568
“dowilliams” <dowilliams@gmail.com>
2022-03-01 09:32:15 -0600

    JSBE assignment

---
+++ IntroToJS/JSBasicsExercise/script.js
@@ -0,0 +1,42 @@
+// JS basics exercise
+// 1
+console.log("47"); //47
+// 2
+console.log("Primitives: strings, booleans, null, undefined, etc."); //console.log("Primitives: strings, booleans, null, undefined, etc.");
+// 3
+// +,-,%,/,**,*
+// 4
+console.log(1%5); // 1
+console.log(1**5); // 1
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+// 6
+console.log("Hawk"+"eye");
+// 7
+console.log("Your Name".length+"Your Last Name".length);
+// 8
+console.log("Avengers: Endgame"[16]); // e
+// Bonus
+// 1
+console.log("\\\\"); // \\
+// 2
+console.log(5**4-9); // 616
+// 3
+console.log("I".length+"Am".length%"Iron".length-"Man".length); // 0
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+// 5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u); // 29
+// 5.777
+let there = be + rock
+// 
