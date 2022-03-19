60f2878742607bd82ea59dab89514cf843a7717c
“dowilliams” <dowilliams@gmail.com>
2022-03-18 19:46:57 -0500

    spread destructuring exercise assignment

---
+++ DataStructures/SpreadDestructuringExercise/script.js
@@ -0,0 +1,56 @@
+// 1a
+const
+
+// 1b
+const starWarsShows = [
+    `The Mandalorian`,
+    `The Book of Boba Fett`
+];
+
+// 1c
+const disneyPlusShows = [
+    ...mcuShows,
+    ...starWarsShows,
+    `The Beatles: Get Back`
+];
+
+// 1d
+console.log(disneyPlusShows);
+
+// 2a
+const netflixMovies = {
+    action: `Extracyion`,
+    crime: `The Irishman`
+};
+
+// 2b
+const amazonPrimeMovies = {
+    action: `The Tomorrow War`,
+    drama: `Onw Night In Miami`
+};
+
+// 2c
+const streamingMovies = {
+    ...amazonPrimeMovies,
+    ...netflixMovies,
+    musical: `Hamilton`
+};
+
+// 2c
+const streamingMovies = {
+    ...amazonPrimeMovies,
+    ...netflixMovies,
+    musical: `Hamilton`
+};
+
+// 2d
+console.log(streamingMovies);
+
+// 2d
+console.log(streamingMovies);
+
+// 2d
+console.log(streamingMovies);
+
+// 2d
+console.log(streamingMovies);
