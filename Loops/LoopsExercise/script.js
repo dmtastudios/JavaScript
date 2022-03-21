729e72aab665c4afaf1335a424d9e936b47779d0
“dowilliams” <dowilliams@gmail.com>
2022-03-21 13:00:16 -0500

    loops exercise assignment

---
+++ Loops/LoopsExercise/script.js
@@ -0,0 +1,42 @@
+// // 3a
+// for wizard = 0{
+//     console.log(wizard);
+// };
+//     `Harry Potter`
+//     `Not Harry Potter`
+//     `Also Not Harry Potter`
+
+// // 3b
+// for (list of wizards) {
+//         console.log(wizards);
+// }
+
+// 4a
+let(HarryPotterMovies) = 1;
+
+while (HarryPotterMovies < 9) {
+    console.log(HarryPotterMovies);
+};
+
+
+const quote = [
+    `Yar`,
+    `Yar2`,
+    `Yar3`,
+];
+
+for (i = 1; i <= 25; i++) {
+    console.log(i);
+    for (x = 3; x <= 25; x + 3) {
+        console.log(`${x}. Expecto`);
+    }
+    for (y = 3; y <= 25; y + 5) {
+        console.log(`${y}. Patronum`);
+    }
+}
+
+for (i = 3; i <= 25; i + 5) {
+    console.log(`${i}. Expecto Patronum`);
+} else if (i % 3 === 0 && i % 5 === 0) {
+
+}
