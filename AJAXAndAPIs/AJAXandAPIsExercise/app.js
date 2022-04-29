c262cc2a9e991c1ca590976f23355c06973ab6cd
“dowilliams” <dowilliams@gmail.com>
2022-04-28 23:52:14 -0500

    ajax and apis exercise

---
+++ AJAXAndAPIs/AJAXandAPIsExercise/app.js
@@ -0,0 +1,26 @@
+// AJAX and APIs Exercise
+
+// 1
+const first = document.querySelector('#first');
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+first.append(p1, p2);
+`{"id":18,"type":"programming","setup":"Why did the programmer quit his job?","punchline":"Because he didn't get arrays."}`;
+
+// 2
+const second = document.querySelector('#second');
+const p3 = document.createElement('p');
+const p4 = document.createElement('p');
+second.append(p3, p4);
+
+// 3
+const third = document.querySelector('#third');
+const p5 = document.createElement('p');
+const p6 = document.createElement('p');
+third.append(p5, p6);
+
+// 4
+const fourth = document.querySelector('#fourth');
+const p7 = document.createElement('p');
+fourth.append(p7);
+
